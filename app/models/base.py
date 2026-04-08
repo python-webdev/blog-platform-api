d48991294528471ce6a1547f4c18e2986e1d7966
Murod Akhmedov <python_webdev@outlook.com>
2026-04-08 05:31:03 +0100

    chore(models): add base model

---
+++ app/models/base.py
@@ -0,0 +1,34 @@
+import uuid
+from datetime import datetime
+
+from sqlalchemy import DateTime, text
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class TimestampMixin:
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        server_default=text("now()"),
+        nullable=False,
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        server_default=text("now()"),
+        onupdate=datetime.now,
+        nullable=False,
+    )
+
+
+class UUIDMixin:
+    id: Mapped[uuid.UUID] = mapped_column(
+        UUID(as_uuid=True),
+        primary_key=True,
+        default=uuid.uuid4,
+        unique=True,
+        nullable=False,
+    )
